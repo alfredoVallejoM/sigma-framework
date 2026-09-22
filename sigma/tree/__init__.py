8ffff590e232d573ff900cb2e55de83fdbe2c6d9
Alfredo Vallejo Martín <146665493+alfredoVallejoM@users.noreply.github.com>
2026-09-22 14:37:20 +0200

    st1: export canonical manifest API

--- sigma/tree/__init__.py
+++ sigma/tree/__init__.py
@@ -2,12 +2,55 @@
 
 from .codec import TreeDecodeError
 from .core import TreeBuilder, build_tree, build_tree_chunks, combine_nodes, empty_root, leaf_node
-from .ids import TreeAlgorithmId, TreeDomainId, TreeProfileId
-from .model import DEFAULT_PROFILE, TreeFrontier, TreeNode, TreeProfileV1, TreeRoot, canonical_frontier_heights
+from .ids import (
+    ManifestEntryKind,
+    ManifestMetadataProfileId,
+    ManifestProfileId,
+    TreeAlgorithmId,
+    TreeDomainId,
+    TreeProfileId,
+)
+from .manifest import (
+    ManifestEntryV1,
+    ManifestV1,
+    SymlinkPolicy,
+    build_directory_manifest,
+    manifest_from_entries,
+)
+from .model import (
+    DEFAULT_PROFILE,
+    TreeFrontier,
+    TreeNode,
+    TreeProfileV1,
+    TreeRoot,
+    canonical_frontier_heights,
+)
+from .path import canonical_relative_path
 
 __all__ = [
-    "DEFAULT_PROFILE", "TreeAlgorithmId", "TreeBuilder", "TreeDecodeError",
-    "TreeDomainId", "TreeFrontier", "TreeNode", "TreeProfileId", "TreeProfileV1",
-    "TreeRoot", "build_tree", "build_tree_chunks", "canonical_frontier_heights",
-    "combine_nodes", "empty_root", "leaf_node",
+    "DEFAULT_PROFILE",
+    "ManifestEntryKind",
+    "ManifestEntryV1",
+    "ManifestMetadataProfileId",
+    "ManifestProfileId",
+    "ManifestV1",
+    "SymlinkPolicy",
+    "TreeAlgorithmId",
+    "TreeBuilder",
+    "TreeDecodeError",
+    "TreeDomainId",
+    "TreeFrontier",
+    "TreeNode",
+    "TreeProfileId",
+    "TreeProfileV1",
+    "TreeRoot",
+    "build_directory_manifest",
+    "build_tree",
+    "build_tree_chunks",
+    "canonical_frontier_heights",
+    "canonical_relative_path",
+    "combine_nodes",
+    "empty_root",
+    "leaf_node",
+    "manifest_from_entries",
 ]
